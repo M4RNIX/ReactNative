f5b9b0086fc527ceaf0ed01de6128542cd544b8d
Marnix <mxdevoogd@gmail.com>
2020-03-12 12:36:32 +0100

    Add all files for Assignment-2

---
+++ Assignment-2/src/index.ts
@@ -0,0 +1,5 @@
+import square from './square';
+import sum from './sum';
+
+document.writeln('The sum of 5 and 8 is ' + sum(5, 8) + '<br>');
+document.writeln('The square of 6 is ' + square(6) + '<br>');
